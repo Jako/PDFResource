f7c38c42d63e34da83d87cb978ee81cc3a17d05f
Thomas Jakobi <thomas.jakobi@partout.info>
2015-11-04 15:29:25 +0100

    Update to 1.2.3 - Bugfix for create_pdf template variable is assigned to the template

--- core/components/pdfresource/elements/plugins/pdfresource.plugin.php
+++ core/components/pdfresource/elements/plugins/pdfresource.plugin.php
@@ -18,21 +18,21 @@ $pdfresource = $modx->getService('pdfresource', 'PDFresource', $pdfresourceCoreP
 $eventName = $modx->event->name;
 switch ($eventName) {
     case 'OnDocFormSave':
-        $modx->switchContext($resource->context_key);
-        $aliasPath = $resource->get('parent') ? preg_replace('#(\.[^./]*)$#', '/', $modx->makeUrl($resource->get('parent'))) : '';
-        $modx->switchContext('mgr');
-
         /** @var modResource $resource */
-        $c = $modx->newQuery('modTemplateVar');
-        $c->leftJoin('modTemplateVarTemplate', 'modTemplateVarTemplate', array('modTemplateVarTemplate.tmplvarid = modTemplateVar.id'));
+        $c = $modx->newQuery('modTemplateVarTemplate');
+        $c->leftJoin('modTemplateVar', 'modTemplateVar', array('modTemplateVarTemplate.tmplvarid = modTemplateVar.id'));
         $c->select($modx->getSelectColumns('modTemplateVar', 'modTemplateVar', '', array('name')));
         $c->select($modx->getSelectColumns('modTemplateVarTemplate', 'modTemplateVarTemplate', '', array('templateid')));
         $c->where(array(
             'modTemplateVar.name' => $pdfresource->getOption('pdfTv', null, 'create_pdf'),
             'modTemplateVarTemplate.templateid' => $resource->get('template')
         ));
-        $assigned = $modx->getObject('modTemplateVar', $c);
+        $assigned = $modx->getObject('modTemplateVarTemplate', $c);
         if ($assigned) {
+            $modx->switchContext($resource->context_key);
+            $aliasPath = $resource->get('parent') ? preg_replace('#(\.[^./]*)$#', '/', $modx->makeUrl($resource->get('parent'))) : '';
+            $modx->switchContext('mgr');
+
             $createPDF = intval($resource->getTVValue($pdfresource->getOption('pdfTv', null, 'create_pdf')));
             if (!$createPDF) {
                 @unlink($pdfresource->getOption('pdfPath') . $aliasPath . $resource->get('alias') . '.pdf');
@@ -43,21 +43,22 @@ switch ($eventName) {
         }
         break;
     case 'OnWebPagePrerender':
-        $aliasPath = $modx->resource->get('parent') ? preg_replace('#(\.[^./]*)$#', '/', $modx->makeUrl($modx->resource->get('parent'))) : '';
-
-        $c = $modx->newQuery('modTemplateVar');
-        $c->leftJoin('modTemplateVarTemplate', 'modTemplateVarTemplate', array('modTemplateVarTemplate.tmplvarid = modTemplateVar.id'));
-        $c->select($modx->getSelectColumns('modTemplateVar', 'modTemplateVar', '', array('name')));
-        $c->select($modx->getSelectColumns('modTemplateVarTemplate', 'modTemplateVarTemplate', '', array('templateid')));
-        $c->where(array(
-            'modTemplateVar.name' => $pdfresource->getOption('pdfTv', null, 'create_pdf'),
-            'modTemplateVarTemplate.templateid' => $modx->resource->get('template')
-        ));
-        $assigned = $modx->getObject('modTemplateVar', $c);
-        if ($assigned) {
-            $createPDF = intval($modx->resource->getTVValue($pdfresource->getOption('pdfTv', null, 'create_pdf')));
-            if ($pdfresource->getOption('generateOnPrerender') && $createPDF && !file_exists($pdfresource->getOption('pdfPath') . $aliasPath . $modx->resource->get('alias') . '.pdf')) {
-                $pdfresource->createPDF($modx->resource, $aliasPath);
+        if ($pdfresource->getOption('generateOnPrerender')) {
+            $c = $modx->newQuery('modTemplateVarTemplate');
+            $c->leftJoin('modTemplateVar', 'modTemplateVar', array('modTemplateVarTemplate.tmplvarid = modTemplateVar.id'));
+            $c->select($modx->getSelectColumns('modTemplateVar', 'modTemplateVar', '', array('name')));
+            $c->select($modx->getSelectColumns('modTemplateVarTemplate', 'modTemplateVarTemplate', '', array('templateid')));
+            $c->where(array(
+                'modTemplateVar.name' => $pdfresource->getOption('pdfTv', null, 'create_pdf'),
+                'modTemplateVarTemplate.templateid' => $modx->resource->get('template')
+            ));
+            $assigned = $modx->getObject('modTemplateVarTemplate', $c);
+            if ($assigned) {
+                $aliasPath = $modx->resource->get('parent') ? preg_replace('#(\.[^./]*)$#', '/', $modx->makeUrl($modx->resource->get('parent'))) : '';
+                $createPDF = intval($modx->resource->getTVValue($pdfresource->getOption('pdfTv', null, 'create_pdf')));
+                if ($createPDF && !file_exists($pdfresource->getOption('pdfPath') . $aliasPath . $modx->resource->get('alias') . '.pdf')) {
+                    $pdfresource->createPDF($modx->resource, $aliasPath);
+                }
             }
         }
         break;
