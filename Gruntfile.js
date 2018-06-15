4d784ad8c60668ee16791051d60ccbac8255d3c1
Thomas Jakobi <thomas.jakobi@partout.info>
2018-06-15 22:16:22 +0200

    Update docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,18 @@ module.exports = function (grunt) {
                         replacement: 'version = \'' + '<%= modx.version %>' + '\''
                     }]
                 }
+            },
+            docs: {
+                files: [{
+                    src: 'mkdocs.yml',
+                    dest: 'mkdocs.yml'
+                }],
+                options: {
+                    replacements: [{
+                        pattern: /&copy; \d{4}(-\d{4})?/g,
+                        replacement: '&copy; ' + (new Date().getFullYear() > 2015 ? '2015-' : '') + new Date().getFullYear()
+                    }]
+                }
             }
         }
     });
