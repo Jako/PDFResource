25ed722634008089be70f9ec24dbf36da7d46fa9
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-27 00:03:26 +0100

    Switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp'),
+    replace = require('gulp-replace'),
+    pkg = require('./_build/config.json');
+
+const year = new Date().getFullYear();
+
+let phpversion;
+let modxversion;
+pkg.dependencies.forEach(function (dependency, index) {
+    switch (pkg.dependencies[index].name) {
+        case 'php':
+            phpversion = pkg.dependencies[index].version.replace(/>=/, '');
+            break;
+        case 'modx':
+            modxversion = pkg.dependencies[index].version.replace(/>=/, '');
+            break;
+    }
+});
+
+gulp.task('bump-copyright', function () {
+    return gulp.src([
+        'core/components/pdfresource/model/pdfresource/pdfresource.class.php',
+        'core/components/pdfresource/src/PDFResource.php'
+    ], {base: './'})
+        .pipe(replace(/Copyright 2015(-\d{4})? by/g, 'Copyright ' + (year > 2015 ? '2015-' : '') + year + ' by'))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump-version', function () {
+    return gulp.src([
+        'core/components/pdfresource/src/PDFResource.php'
+    ], {base: './'})
+        .pipe(replace(/version = '\d+\.\d+\.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump-docs', function () {
+    return gulp.src([
+        'mkdocs.yml',
+    ], {base: './'})
+        .pipe(replace(/&copy; 2015(-\d{4})?/g, '&copy; ' + (year > 2015 ? '2015-' : '') + year))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump-requirements', function () {
+    return gulp.src([
+        'docs/index.md',
+    ], {base: './'})
+        .pipe(replace(/[*-] MODX Revolution \d.\d.*/g, '* MODX Revolution ' + modxversion + '+'))
+        .pipe(replace(/[*-] PHP (v)?\d.\d.*/g, '* PHP ' + phpversion + '+'))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-docs', 'bump-requirements'));
+
+// Default Task
+gulp.task('default', gulp.series('bump'));
