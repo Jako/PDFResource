8a1db61e7a26924addcd613764c2632cdaf85396
Thomas Jakobi <thomas.jakobi@partout.info>
2023-12-12 13:11:56 +0100

    Change statistics resolver from url/ip to uuid

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -23,7 +23,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         developed by Treehill Studio - MODX development in Münsterland.</p>
 
         <p>During the installation, we will collect some statistical data (the
-        hostname, the IP address, the PHP version and the MODX version of your
+        hostname, the MODX UUID, the PHP version and the MODX version of your
         MODX installation). Your data will be kept confidential and under no
         circumstances be used for promotional purposes or disclosed to third
         parties. We only like to know the usage count of this package.</p>
@@ -46,12 +46,11 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         `{core_path}/components/pdfresource/vendor/mpdf/mpdf/ttfonts` before
         updating.</p>
 
-        During the upgrade, we will collect some statistical data (the hostname,
-        the IP address, the PHP version, the MODX version of your MODX
-        installation and the previous installed version of this extra package).
-        Your data will be kept confidential and under no circumstances be used
-        for promotional purposes or disclosed to third parties. We only like to
-        know the usage count of this package.</p>
+        <p>During the installation, we will collect some statistical data (the
+        hostname, the MODX UUID, the PHP version and the MODX version of your
+        MODX installation). Your data will be kept confidential and under no
+        circumstances be used for promotional purposes or disclosed to third
+        parties. We only like to know the usage count of this package.</p>
 
         <p>If you upgrade this package, you are giving us your permission to
         collect, process and use that data for statistical purposes.</p>';
